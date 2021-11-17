7394625e1e826236d1e0adc341f84158bd258896
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2021-11-17 18:46:29 +0100

    change imports

--- src/preprocess_create_df_from_signals.py
+++ src/preprocess_create_df_from_signals.py
@@ -1,4 +1,3 @@
-
 from mne.epochs import Epochs
 from preprocess_unzip_data import unzip_data
 from mne import make_fixed_length_epochs
@@ -29,35 +28,45 @@ from utils_env import *
 from utils_functions import *
 from utils_entropy import *
 
-set_option('display.max_columns', None)
-warnings.filterwarnings('ignore')
+set_option("display.max_columns", None)
+warnings.filterwarnings("ignore")
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--users', metavar='N', type=int,
-                    help='Number of users that will be used (<= 12)')
+parser = argparse.ArgumentParser(description="Process some integers.")
+parser.add_argument(
+    "--users",
+    metavar="N",
+    type=int,
+    help="Number of users that will be used (<= 12)",
+)
 
-parser.add_argument('--electrodes', metavar='N', type=int, nargs=2,
-                    help='Range of electrodes that will be used (N <= 30)')
+parser.add_argument(
+    "--electrodes",
+    metavar="N",
+    type=int,
+    nargs=2,
+    help="Range of electrodes that will be used (N <= 30)",
+)
 
-parser.add_argument('--sig', metavar='N', type=int,
-                    help='Duration of signal (<= 300)')
-parser.add_argument('--epoch_elems', metavar='N', type=int,
-                    help='Duration of signal (<= 1000)')
+parser.add_argument(
+    "--sig", metavar="N", type=int, help="Duration of signal (<= 300)"
+)
+parser.add_argument(
+    "--epoch_elems", metavar="N", type=int, help="Duration of signal (<= 1000)"
+)
 
-parser.add_argument('--unzip', metavar='N', type=bool,
-                    help='True/False')
+parser.add_argument("--unzip", metavar="N", type=bool, help="True/False")
 args = parser.parse_args()
 
 is_complete_itteration = len(sys.argv) == 1
 
 
 USER_COUNT = args.users if (args.users) else USER_COUNT
-SIGNAL_REQUESTED_SECONDS = args.sig if (
-    args.sig) else SIGNAL_REQUESTED_SECONDS
-if (args.electrodes):
-    subset = elect_cols[args.electrodes[0]:args.electrodes[1]]
+SIGNAL_REQUESTED_SECONDS = args.sig if (args.sig) else SIGNAL_REQUESTED_SECONDS
+if args.electrodes:
+    subset = elect_cols[args.electrodes[0] : args.electrodes[1]]
     elect_cols_ignore = [
-        electrode for electrode in elect_cols if electrode not in subset]
+        electrode for electrode in elect_cols if electrode not in subset
+    ]
     elect_cols = subset
 
 epoch_elems = args.epoch_elems if args.epoch_elems else FREQ
@@ -68,25 +77,37 @@ if args.unzip:
 
 def get_epochs_from_signal(filename: str):
     eeg = read_raw_cnt(
-        filename, eog=['HEOL', "HEOR", "VEOU", "VEOL"], preload=True, verbose=False)
-    eeg_filtered = eeg.notch_filter(
-        50).filter(l_freq=0.15, h_freq=40)
+        filename,
+        eog=["HEOL", "HEOR", "VEOU", "VEOL"],
+        preload=True,
+        verbose=False,
+    )
+    eeg_filtered = eeg.notch_filter(50).filter(l_freq=0.15, h_freq=40)
     signal_seconds_floored = floor(len(eeg_filtered) / FREQ)
-    tmin = signal_seconds_floored - SIGNAL_REQUESTED_SECONDS - SAFETY_CUTOFF_SECONDS
+    tmin = (
+        signal_seconds_floored
+        - SIGNAL_REQUESTED_SECONDS
+        - SAFETY_CUTOFF_SECONDS
+    )
     tmax = signal_seconds_floored - SAFETY_CUTOFF_SECONDS
     eeg_filtered = eeg_filtered.crop(tmin=tmin, tmax=tmax)
-    return make_fixed_length_epochs(eeg, duration=EPOCH_SECONDS, preload=False, verbose=False)
+    return make_fixed_length_epochs(
+        eeg, duration=EPOCH_SECONDS, preload=False, verbose=False
+    )
 
 
 def get_df_from_epochs(epochs: Epochs, state: str):
     df: DataFrame = epochs.to_data_frame(scalings=dict(eeg=1, mag=1, grad=1))
-    df = df.drop(['time', 'condition', * elect_cols_ignore], axis=1)
+    df = df.drop(["time", "condition", *elect_cols_ignore], axis=1)
     return df
 
 
 # {(0,normal), (0,fatigue), (1,normal)...(12,fatigue)}
-user_state_pairs = [(i_user, state) for i_user in range(
-    0, USER_COUNT) for state in [NORMAL_STR, FATIGUE_STR]]
+user_state_pairs = [
+    (i_user, state)
+    for i_user in range(0, USER_COUNT)
+    for state in [NORMAL_STR, FATIGUE_STR]
+]
 
 arr_users = []
 entropy_order = ["PE", "AE", "SE", "FE"]
@@ -111,13 +132,21 @@ for pair in user_state_pairs:
         df_electrodes = df_epoch[elect_cols]
 
         df_spectral_entropy = df_electrodes.apply(
-            func=lambda x: pd_spectral_entropy(x, freq=FREQ, standardize_input=True), axis=0)  # type: ignore
+            func=lambda x: pd_spectral_entropy(
+                x, freq=FREQ, standardize_input=True
+            ),
+            axis=0,
+        )  # type: ignore
         df_approximate_entropy = df_electrodes.apply(
-            func=lambda x: pd_approximate_entropy(x, standardize_input=True), axis=0)  # type: ignore
+            func=lambda x: pd_approximate_entropy(x, standardize_input=True),
+            axis=0,
+        )  # type: ignore
         df_sample_entropy = df_electrodes.apply(
-            func=lambda x: pd_sample_entropy(x, standardize_input=True), axis=0)  # type: ignore
+            func=lambda x: pd_sample_entropy(x, standardize_input=True), axis=0
+        )  # type: ignore
         df_fuzzy_entropy = df_electrodes.apply(
-            func=lambda x: pd_fuzzy_entropy(x, standardize_input=True), axis=0)  # type: ignore
+            func=lambda x: pd_fuzzy_entropy(x, standardize_input=True), axis=0
+        )  # type: ignore
 
         df_dict = {
             "PE": df_spectral_entropy,
@@ -127,11 +156,21 @@ for pair in user_state_pairs:
         }
 
         # [0, PE_FP1, PE_FP2, ... , PE_C3, AE_FP1, AE_FP2, ..., FE_C3]
-        arr_users.append([label, *df_dict[entropy_order[0]], *df_dict[entropy_order[1]],
-                         *df_dict[entropy_order[2]], *df_dict[entropy_order[3]]])
+        arr_users.append(
+            [
+                label,
+                *df_dict[entropy_order[0]],
+                *df_dict[entropy_order[1]],
+                *df_dict[entropy_order[2]],
+                *df_dict[entropy_order[3]],
+            ]
+        )
 
-entropy_electrode_combinations = ["{}_{}".format(entropy, electrode)
-                                  for entropy in entropy_order for electrode in elect_cols]
+entropy_electrode_combinations = [
+    "{}_{}".format(entropy, electrode)
+    for entropy in entropy_order
+    for electrode in elect_cols
+]
 
 columns = ["label"] + entropy_electrode_combinations
 df = DataFrame(arr_users, columns=columns)
@@ -142,12 +181,13 @@ def clean_df(df: DataFrame):
     # set to 0 if treshold is met
     # NaN entropies can be set to zero
     # standard scaler scales for each column independently
-    threshold = 1E-6
+    threshold = 1e-6
     df[df < threshold] = 0
     df[df <= 0] = 0
     df = df.fillna(0)
     df[entropy_electrode_combinations] = min_max_scaler(
-        df[entropy_electrode_combinations])
+        df[entropy_electrode_combinations]
+    )
     return df
 
 
@@ -157,7 +197,8 @@ glimpse_df(df)
 X = df.loc[:, ~df.columns.isin(["label"])]
 y = df.loc[:, "label"]
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.5, random_state=0)
+    X, y, test_size=0.5, random_state=0
+)
 
 tuned_parameters = [
     {"kernel": ["rbf"], "gamma": [1e-3, 1e-4], "C": [1, 10, 100, 1000]},
---
+++ src/visual_entropy_survey.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from typing import Dict
+import scipy.io
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from utils_paths import PATH_DATA_MAT
+
+from utils_entropy import dict_apply_procedture, isnull_values_any
+from utils_functions import min_max_dataframe, standard_scale_dataframe
+
+mat = scipy.io.loadmat(Path(PATH_DATA_MAT, "1.mat"))
+keys = [key for key in mat.keys() if not key.startswith("__")]
+
+[print(key, "with shape", mat[key].shape) for key in keys]
+
+is_normal_state_mask = pd.Series(mat["Class_label"].squeeze() == 0)
+keys_entropy = ["FE", "SE", "AE", "PE"]
+
+print(is_normal_state_mask)
+
+entropies: Dict[str, pd.DataFrame] = {}
+for key in keys_entropy:
+    entropies[key] = pd.DataFrame(mat[key])
+    print(key, "\n", entropies[key].describe())
+
+
+fig = plt.figure(figsize=(20, 20), tight_layout={"h_pad": 2})
+
+ax = fig.add_subplot(3, 1, 1)
+for name, entropy in entropies.items():
+    ax.set_title("Original")
+    ax.scatter(entropy.index.tolist(), entropy.loc[:, 1], label=name)
+    ax.legend()
+
+entropies_scaled = dict_apply_procedture(entropies, min_max_dataframe)
+ax = fig.add_subplot(3, 1, 2)
+for name, entropy in entropies_scaled.items():
+    ax.set_title("Min_max")
+    ax.scatter(entropy.index.tolist(), entropy.loc[:, 1], label=name)
+    ax.legend()
+
+entropies_scaled_2 = dict_apply_procedture(entropies, standard_scale_dataframe)
+ax = fig.add_subplot(3, 1, 3)
+for name, entropy in entropies_scaled_2.items():
+    ax.set_title("Standard scaler")
+    ax.scatter(entropy.index.tolist(), entropy.loc[:, 1], label=name)
+    ax.legend()
+plt.show()
+
+# Null and NaN are the same in Pandas :)
+
+
+for name, entropy in entropies_scaled.items():
+    if isnull_values_any(entropy):
+        print("Entropy", name, "has null values")
+    if isnull_values_any(entropy):
+        print("Entropy", name, "has null values")
